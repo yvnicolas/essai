755666baa41a5a506cb62930a2699f8db7e6a1b3
yvnicolas <yves.nicolas@dynamease.com>
2013-06-04 10:08:25 +0200

    Je mets un nouveau répertoire créé de 0

---
+++ jemetsunnouveaurepertoire/unEssai/src/ClasseDeRienDuTout.java
@@ -0,0 +1,12 @@
+
+public class ClasseDeRienDuTout {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Coucou les aminches");
+	}
+
+}
