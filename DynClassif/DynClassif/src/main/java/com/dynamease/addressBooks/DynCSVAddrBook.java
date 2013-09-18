5ac3a3959dfdb8403207786ab43a8d6791539ebb
Yves Nicolas <yves.nicolas@dynamease.com>
2013-09-18 12:27:42 +0200

    Clean up DynCSVAddrBook

--- DynClassif/DynClassif/src/main/java/com/dynamease/addressBooks/DynCSVAddrBook.java
+++ DynClassif/DynClassif/src/main/java/com/dynamease/addressBooks/DynCSVAddrBook.java
@@ -28,198 +28,187 @@ import com.dynamease.entity.DynSubscriber;
 
 public class DynCSVAddrBook extends DynExternalAddressBookImpl {
 
-	private static final String CATSEP = "[:;*]";
+    private static final String CATSEP = "[:;*]";
 
-	private Logger logger = LoggerFactory.getLogger(DynCSVAddrBook.class);
+    private Logger logger = LoggerFactory.getLogger(DynCSVAddrBook.class);
 
-	
-	private List<DynCSVContact> dynCSVContacts;
+    private List<DynCSVContact> dynCSVContacts;
 
-	private int currentIndex;
+    private int currentIndex;
 
-	private Set<String> categories;
+    private Set<String> categories;
 
-	/**
-	 * Creates a new Dynamease Address Book from a CSV file Normalizes the
-	 * header
-	 * 
-	 * @param linkToFile
-	 */
-	public DynCSVAddrBook(File linkToFile) {
-		super();
-		initCsvAddrBook(linkToFile);
-	}
+    /**
+     * Creates a new Dynamease Address Book from a CSV file Normalizes the
+     * header
+     * 
+     * @param linkToFile
+     */
+    public DynCSVAddrBook(File linkToFile) {
+        super();
+        initCsvAddrBook(linkToFile);
+    }
 
-	// Attention : this code does not work with InputStream Reader (SuperCSV
-	// problem)
-	private void initCsvAddrBook(File linkToFile) {
+    // Attention : this code does not work with InputStream Reader (SuperCSV
+    // problem)
+    private void initCsvAddrBook(File linkToFile) {
 
-		categories = new HashSet<String>();
+        categories = new HashSet<String>();
 
-		String[] csvHeader = null;
-		DynHeaderNormalizer headerDico = new DynHeaderNormalizer();
+        String[] csvHeader = null;
+        DynHeaderNormalizer headerDico = new DynHeaderNormalizer();
 
-		// Reading the Header. A CsvListReader object is used here as it can
-		// read a variable number of columns in the first line (see
-		// http://supercsv.sourceforge.net/readers.html)
-		CsvListReader listReader = null;
-		InputStreamReader b = null;
-		try {
-			b = new InputStreamReader(new BufferedInputStream(new FileInputStream(linkToFile)));
-			listReader = new CsvListReader(b, CsvPreference.STANDARD_PREFERENCE);
-			csvHeader = listReader.getHeader(true);
-		} catch (IOException e) {
-			logger.info("Did not manage to get the Csv Header", e);
-			try {
-				listReader.close();
-			} catch (IOException e1) {
-				logger.info("Problem trying to close the readers", e1);
-				return;
-			}
+        // Reading the Header. A CsvListReader object is used here as it can
+        // read a variable number of columns in the first line (see
+        // http://supercsv.sourceforge.net/readers.html)
+        CsvListReader listReader = null;
+        InputStreamReader b = null;
+        try {
+            b = new InputStreamReader(new BufferedInputStream(new FileInputStream(linkToFile)));
+            listReader = new CsvListReader(b, CsvPreference.STANDARD_PREFERENCE);
+            csvHeader = listReader.getHeader(true);
+        } catch (IOException e) {
+            logger.info("Did not manage to get the Csv Header", e);
+        } finally {
+            try {
+                listReader.close();
+            } catch (IOException e1) {
+                logger.info("Problem trying to close the readers", e1);
+                return;
+            }
+        }
 
-		}
+        // Header Normalization
+        for (int i = 0; i < csvHeader.length; i++) {
+            logger.info(String.format("Element %d du header :  %s", i, csvHeader[i]));
+            csvHeader[i] = headerDico.lookup(csvHeader[i]);
+        }
 
-		// Header Normalization
-		for (int i = 0; i < csvHeader.length; i++) {
-			logger.info(String.format("Element %d du header :  %s", i,
-					csvHeader[i]));
-			csvHeader[i] = headerDico.lookup(csvHeader[i]);
-		}
+        // Trace the header after Normalization
+        if (logger.isDebugEnabled()) {
+            logger.info("Header apres normalisation");
+            for (int i = 0; i < csvHeader.length; i++) {
+                if (csvHeader[i] != null) {
+                    logger.info(String.format("Element %d du header :  %s", i, csvHeader[i]));
+                }
+            }
+        }
 
-		// Trace the header after Normalization
-		if (logger.isDebugEnabled()) {
-			logger.info("Header apres normalisation");
-			for (int i = 0; i < csvHeader.length; i++) {
-				if (csvHeader[i] != null) {
-					logger.info(String.format("Element %d du header :  %s", i,
-							csvHeader[i]));
-				}
-			}
-		}
+        // Using the CSV bean reader to read the file. Now we know the number of
+        // columns
+        // A CsvBeanReader object is the choice to extract easier to POJO
+        // structure
+        CsvBeanReader beanReader = null;
 
-		// Using the CSV bean reader to read the file. Now we know the number of
-		// columns
-		// A CsvBeanReader object is the choice to extract easier to POJO
-		// structure
-		CsvBeanReader beanReader = null;
+        try {
+            b = new InputStreamReader(new BufferedInputStream(new FileInputStream(linkToFile)));
+            beanReader = new CsvBeanReader(b, CsvPreference.STANDARD_PREFERENCE);
+            // beanReader starts reading from line 2 (see above)
+            // it is as if we would be reading a file without a header
+            beanReader.getHeader(false);
 
-		try {
-			b = new InputStreamReader(new BufferedInputStream(new FileInputStream(linkToFile)));
-			beanReader = new CsvBeanReader(b, CsvPreference.STANDARD_PREFERENCE);
-			// beanReader starts reading from line 2 (see above)
-			// it is as if we would be reading a file without a header
-			beanReader.getHeader(false);
+            dynCSVContacts = extractCSVContacts(beanReader, csvHeader);
 
-			dynCSVContacts = extractCSVContacts(beanReader, csvHeader);
+        } catch (IOException e) {
+            logger.info("Did not manage to get a working CsvBeanReader.", e);
+           return;
+        }
+        finally {
+            try {
+                beanReader.close();
+            } catch (IOException e1) {
+                logger.info("Problem trying to close the readers", e1);
+            }
+        }
 
-		} catch (IOException e) {
-			logger.info("Did not manage to get a working CsvBeanReader.", e);
-			try {
-				beanReader.close();
-			} catch (IOException e1) {
-				logger.info("Problem trying to close the readers", e1);
-			}
-			return;
-		}
+        currentIndex = 0;
 
-		currentIndex = 0;
+    }
 
-		try {
-			beanReader.close();
+    @Override
+    public boolean hasNext() {
+        return (currentIndex < dynCSVContacts.size());
+    }
 
-		} catch (IOException e) {
-			logger.info("Problem trying to close the readers", e);
-		}
+    @Override
+    public Set<String> retreiveCategories() {
+        return categories;
+    }
 
-	}
+    @Override
+    public DynPerson getNextEntry() {
 
-	@Override
-	public boolean hasNext() {
-		return (currentIndex < dynCSVContacts.size());
-	}
+        DynPerson toReturn = (DynPerson) dynCSVContacts.get(currentIndex);
+        currentIndex++;
+        return toReturn;
+    }
 
-	@Override
-	public Set<String> retreiveCategories() {
-		return categories;
-	}
+    @Override
+    public int rateCurrent(DynCategories cat, DynSubscriber sub) {
+        DynCSVContact current = dynCSVContacts.get(currentIndex);
+        StringBuilder sb = new StringBuilder(".*");
+        sb.append(cat.toString());
+        sb.append(".*");
+        if (current.getCategories().toUpperCase().matches(sb.toString()))
+            return 100;
+        else
+            return 0;
+    }
 
-	@Override
-	public DynPerson getNextEntry() {
+    private CellProcessor[] constructProcessors(String[] csvHeader) {
 
-		DynPerson toReturn = (DynPerson) dynCSVContacts.get(currentIndex);
-		currentIndex++;
-		return toReturn;
-	}
+        // In this implementation, no mandatory field are taken into account
+        // This method just returns the right cell processors array with the
+        // right number
 
-	@Override
-	public int rateCurrent(DynCategories cat, DynSubscriber sub) {
-		DynCSVContact current = dynCSVContacts.get(currentIndex);
-		StringBuilder sb = new StringBuilder(".*");
-		sb.append(cat.toString());
-		sb.append(".*");
-		if (current.getCategories().toUpperCase().matches(sb.toString()))
-			return 100;
-		else
-			return 0;
-	}
+        ArrayList<CellProcessor> processorsAsList = new ArrayList<CellProcessor>(20);
+        for (int i = 0; i < csvHeader.length; i++) {
+            processorsAsList.add(new Optional());
+        }
 
-	private CellProcessor[] constructProcessors(String[] csvHeader) {
+        CellProcessor result[] = new CellProcessor[processorsAsList.size()];
+        return processorsAsList.toArray(result);
 
-		// In this implementation, no mandatory field are taken into account
-		// This method just returns the right cell processors array with the
-		// right number
+    }
 
-		ArrayList<CellProcessor> processorsAsList = new ArrayList<CellProcessor>(
-				20);
-		for (int i = 0; i < csvHeader.length; i++) {
-			processorsAsList.add(new Optional());
-		}
+    /**
+     * Extracts DynCSVContacts from the beanReader. Retrieves the list of
+     * categories for this address book from the categories field.
+     * 
+     * @param beanReader
+     * @return A list of DynCSVContact
+     */
+    private List<DynCSVContact> extractCSVContacts(ICsvBeanReader beanReader, String[] csvHeader) {
 
-		CellProcessor result[] = new CellProcessor[processorsAsList.size()];
-		return processorsAsList.toArray(result);
+        List<DynCSVContact> contacts = new ArrayList<>();
+        DynCSVContact contact;
+        CellProcessor[] processors = constructProcessors(csvHeader);
+        try {
+            while ((contact = beanReader.read(DynCSVContact.class, csvHeader, processors)) != null) {
+                extractCategories(contact.getCategories());
+                logger.info(String.format("Contact : %s %s", contact.getFirstName(), contact.getLastName()));
+                contacts.add(contact);
+            }
+        } catch (IOException e) {
+            logger.info("There seems to be a trouble with the file.", e);
+        }
+        return contacts;
+    }
 
-	}
+    private void extractCategories(String catStringList) {
 
-	/**
-	 * Extracts DynCSVContacts from the beanReader. Retrieves the list of
-	 * categories for this address book from the categories field.
-	 * 
-	 * @param beanReader
-	 * @return A list of DynCSVContact
-	 */
-	private List<DynCSVContact> extractCSVContacts(ICsvBeanReader beanReader,
-			String[] csvHeader) {
+        // Categories might be found in a single column separated
+        // Extract the individual categorie namses
+        String[] catSepares = catStringList.split(CATSEP);
+        for (String cat : catSepares) {
+            // Remove Starting and ending white spaces
+            cat = cat.replaceAll("^\\s", "");
+            cat = cat.replaceAll("\\s$", "");
+            if (!cat.equals("")) {
+                categories.add(cat);
+            }
+        }
 
-		List<DynCSVContact> contacts = new ArrayList<>();
-		DynCSVContact contact;
-		CellProcessor[] processors = constructProcessors(csvHeader);
-		try {
-			while ((contact = beanReader.read(DynCSVContact.class, csvHeader,
-					processors)) != null) {
-				extractCategories(contact.getCategories());
-				logger.info(String.format("Contact : %s %s", contact.getFirstName(), contact.getLastName()));
-				contacts.add(contact);
-			}
-		} catch (IOException e) {
-			logger.info("There seems to be a trouble with the file.", e);
-		}
-		return contacts;
-	}
-
-	private void extractCategories(String catStringList) {
-
-		// Categories might be found in a single column separated
-		// Extract the individual categorie namses
-		String[] catSepares = catStringList.split(CATSEP);
-		for (String cat : catSepares) {
-			// Remove Starting and ending white spaces
-			cat = cat.replaceAll("^\\s", "");
-			cat = cat.replaceAll("\\s$", "");
-			if (!cat.equals("")) {
-				categories.add(cat);
-			}
-		}
-
-	}
+    }
 
 }
